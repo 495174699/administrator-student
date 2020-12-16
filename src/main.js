711efc39bb1aa1565d2469f86f43060dfeab19d0
495174699 <495174699@qq.com>
2020-12-16 13:16:39 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
+/**
+ * 项目启动入口
+ */
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-Vue.config.productionTip = false
+// 加载全局样式文件
+import './style/index.less'
 
+Vue.config.productionTip = false
+    /**
+     * 创建vue根实列
+     * 把router配置到根里面
+     * 通过 render 方法把 App根组件渲染到 #app 入口节点
+     */
 new Vue({
-  router,
-  render: h => h(App)
+    router,
+    render: h => h(App)
 }).$mount('#app')
